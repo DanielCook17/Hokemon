b7cffa517f049edea5049574f513d5548fdae570
Daniel Cook <COO202009586@longroad.ac.uk>
2021-01-13 11:48:52 +0000

    Add project files.

---
+++ Hokemon/Hokemon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon
+{
+    class Hokemon
+    {
+        // Hokemon attributes
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+        // CONSTRUCTOR METHOD FOR THE HOKEMON
+        public Hokemon()
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+        }
+
+        public string get_name()
+        {
+            return name;
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("***************\nName: {0} \nHealth: {1}/{2} \nAttack: {3} \nSpeed: {4} \nDefense {5} \n***************\n", name, health, max_health, attack, speed, defense);
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("Enter the name of the Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public int random_int_generator(int min_value, int max_value) // PARAMETERS (int min_value, int max_value)
+        {
+            Random rnd = new Random();
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);
+
+            return randomValue;
+        }
+    }
+}
---
+++ Hokemon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Hokeworld home of the Hokemon");
+
+            // Instantiation of new Hokemon
+
+            Hokemon hoke01 = new Hokemon(); // Instantiate new Hokemon object to as Hoke01 
+
+            hoke01.get_details();
+
+            Hokemon hoke02 = new Hokemon();
+
+            hoke02.get_details();
+
+            Hokemon hoke03 = new Hokemon();
+
+            hoke03.get_details();
+        }
+    }
+}
