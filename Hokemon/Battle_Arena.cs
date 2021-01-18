bf81cd2f60adbccf41333da5593b534b5f6564a4
Daniel Cook <COO202009586@longroad.ac.uk>
2021-01-18 13:09:53 +0000

    Hokemon

--- Hokemon/Battle_Arena.cs
+++ Hokemon/Battle_Arena.cs
@@ -33,22 +33,66 @@ namespace Hokemon
                 string[] attackMoves = { "Punches", "Bites", "Scratches", "Pummels", "Hurts", "Digs under" };
                 Random rnd = new Random();
                 int randomValue;
-                randomValue = rnd.Next(0, 5);
+                randomValue = rnd.Next(0, 6);
                 chosenAttackMove = attackMoves[randomValue];
+
+               
+                Random attackCalcrnd = new Random();
+                int attackCalcRandomValue;
+                attackCalcRandomValue = attackCalcrnd.Next(1, 5);
+
+                Random defendCalcrnd = new Random();
+                int defendCalcRandomValue;
+                defendCalcRandomValue = defendCalcrnd.Next(2, 8);
+
+              
 
 
                 if (attacker.attackCalculator() > defender.defenceCalculator())
                 {
+                    Console.WriteLine("{0} {1} {2}", attacker.Name, chosenAttackMove, defender.Name);
                     int damage;
                     damage = (attacker.attackCalculator() - defender.defenceCalculator());
                     defender.Health = defender.Health - damage;
+                    Console.WriteLine("The attack dealt {0} damage!. {1} has {2} health left!", damage, defender.Name, defender.Health);
                     if (defender.Health < 1)
                     {
-                        X = false;
-                    }
-                   
+                        Console.WriteLine("Your {0} has died! You lose", defender.Name);
+                        X = true;
+
+                    } 
                 }
                 else
+                {
+                    Console.WriteLine("{0}'s attack failed", attacker.Name);
+                }
+
+                string defenderChosenAttackMove;
+                string[] defenderAttackMoves = { "Punches", "Bites", "Scratches", "Pummels", "Hurts", "Digs under" };
+                Random defRnd = new Random();
+                int defRandomValue;
+                defRandomValue = defRnd.Next(0, 6);
+                defenderChosenAttackMove = defenderAttackMoves[defRandomValue];
+
+                if (defender.attackCalculator() > attacker.defenceCalculator())
+                {
+                    Console.WriteLine("{0} {1} {2}", defender.Name, chosenAttackMove, attacker.Name);
+                    int damage;
+                    damage = (defender.attackCalculator() - attacker.defenceCalculator());
+                    attacker.Health = attacker.Health - damage;
+                    Console.WriteLine("The attack dealt {0} damage!. {1} has {2} health left!", damage, attacker.Name, attacker.Health);
+                    if (attacker.Health < 1)
+                    {
+                        Console.WriteLine("{0} has died! You win", attacker.Name);
+                        X = true;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0}'s attack failed", defender.Name);
+                }
+
             }
         }
     }
